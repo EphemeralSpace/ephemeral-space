59bd4ae1e02cf488cf28e15d5b5cb4293343f1cd
mirrorcult <mirrorcult@pm.me>
2025-09-08 19:45:50 -0700

    oh.. thats peak.. thats peak of my comfort character

--- Content.Client/_ES/Wallmount/ESWallMountVisibilityOverlay.cs
+++ Content.Client/_ES/Wallmount/ESWallMountVisibilityOverlay.cs
@@ -1,12 +1,16 @@
 using System.Numerics;
 using Content.Client._ES.Wallmount.Systems;
-using Content.Shared.Wall;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
 
 namespace Content.Client._ES.Wallmount;
 
+/// <summary>
+///     Renders wallmount visibility based on their facing direction and position relative to the center of a viewport's eye.
+///     This abuses the fact that sprite render commands (like setting visibility) are not batched in any way, and we can
+///     just set the visibility to something else mid-render
+/// </summary>
 public sealed class ESWallMountVisibilityOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _ent = default!;
@@ -28,10 +32,10 @@ public sealed class ESWallMountVisibilityOverlay : Overlay
 
     protected override void Draw(in OverlayDrawArgs args)
     {
-        if (args.ViewportControl == null || args.Viewport.Eye == null)
+        if (args.Viewport.Eye == null)
             return;
 
-        var matrix = args.ViewportControl.GetWorldToScreenMatrix();
+        var matrix = args.Viewport.GetWorldToLocalMatrix();
         var entities = _tree.QueryAabb(args.MapId, args.WorldBounds);
 
         foreach (var entry in entities)
@@ -53,18 +57,17 @@ public sealed class ESWallMountVisibilityOverlay : Overlay
                 continue;
 
             var (pos, rot) = _xform.GetWorldPositionRotation(xform);
+
             // we figure out which wallmounts should be visible based on their direction & rotation adjusted for eye rotation
-            // + their position relative to the player's screencoords (the four quadrants surrounding them), which is
-            // usually the center of the screen but doesn't necessarily need to be
+            // + their position relative to the viewport center's screencoords (the four quadrants surrounding them)
             var wallmountScreenRotation = rot + args.Viewport.Eye.Rotation + wallmount.Direction;
 
-            // measure how much the wallmount angle is 'facing' the player
-            // if its < 90deg then it should be visible
-
             var entityScreenPos = Vector2.Transform(pos, matrix);
-            var viewportCenterScreenPos = args.ViewportBounds.Center;
+            var eyeScreenPos = Vector2.Transform(args.Viewport.Eye.Position.Position, matrix); // there is surely a better way to get this value from somewhere
+            var dist = (entityScreenPos - eyeScreenPos);
 
-            var dist = (entityScreenPos - viewportCenterScreenPos);
+            // measure how much the wallmount angle is 'facing' the viewport center
+            // if its < 90deg then it should be visible
             // i have no fucking idea why i need to flip x, genuinely
             // but it fixes the math. it worked fine vertically
             var distAngle = (dist with { X = -dist.X }).ToWorldAngle();
