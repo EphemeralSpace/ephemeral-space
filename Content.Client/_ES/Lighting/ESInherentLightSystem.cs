f69bd3ca9f5c3449396138492fb04bf9fdf11c08
mirrorcult <mirrorcult@pm.me>
2025-09-14 01:08:21 -0700

    Clientside inherent mob lights

---
+++ Content.Client/_ES/Lighting/ESInherentLightSystem.cs
@@ -0,0 +1,46 @@
+using System.Numerics;
+using Robust.Client.GameObjects;
+using Robust.Shared.Map;
+using Robust.Shared.Player;
+
+namespace Content.Client._ES.Lighting;
+
+/// <summary>
+///     Handles enabling and disabling mob inherent pointlights when locally attaching to a new mob.
+/// </summary>
+public sealed class ESInherentLightSystem : EntitySystem
+{
+    [Dependency] private readonly PointLightSystem _light = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<LocalPlayerAttachedEvent>(OnPlayerAttach);
+        SubscribeLocalEvent<LocalPlayerDetachedEvent>(OnPlayerDetach);
+    }
+
+    private void OnPlayerAttach(LocalPlayerAttachedEvent ev)
+    {
+        if (!TryComp<ESInherentLightComponent>(ev.Entity, out var light)
+            || light.LightEntity != null)
+            return;
+
+        // Don't enable inherent light if the mob already has a pointlight on itself
+        if (TryComp<PointLightComponent>(ev.Entity, out var selfPointLight) && selfPointLight.Enabled)
+            return;
+
+        light.LightEntity = SpawnAttachedTo(light.LightPrototype, new EntityCoordinates(ev.Entity, Vector2.Zero));
+        _light.SetEnabled(light.LightEntity.Value, true);
+    }
+
+    private void OnPlayerDetach(LocalPlayerDetachedEvent ev)
+    {
+        if (!TryComp<ESInherentLightComponent>(ev.Entity, out var light)
+            || light.LightEntity == null
+            || !TryComp<PointLightComponent>(light.LightEntity, out var inherentPointLight))
+            return;
+
+        QueueDel(light.LightEntity);
+        light.LightEntity = null;
+    }
+}
