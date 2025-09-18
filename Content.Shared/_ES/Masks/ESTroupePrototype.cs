d202ed7f43e0e8299d5c0e016b77fa5d3c264990
EmoGarbage404 <retron404@gmail.com>
2025-09-18 19:11:31 -0400

    Add troupe names, Fix test fail

--- Content.Shared/_ES/Masks/ESTroupePrototype.cs
+++ Content.Shared/_ES/Masks/ESTroupePrototype.cs
@@ -18,6 +18,12 @@ public sealed partial class ESTroupePrototype : IPrototype, IInheritingPrototype
     public bool Abstract { get; }
 
     /// <summary>
+    /// Name of the troupe, in plain text.
+    /// </summary>
+    [DataField(required: true)]
+    public LocId Name;
+
+    /// <summary>
     /// Players with any of these jobs will be ineligible for being members of this troupe
     /// </summary>
     [DataField]
