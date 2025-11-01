64abac998ea95abb061b7a919b09792c0ff74f2c
mirrorcult <mirrorcult@pm.me>
2025-11-01 13:10:35 -0700

    Poster stuff part 1

---
+++ Content.Client/_ES/Wallmount/Posters/ESPosterComponent.cs
@@ -0,0 +1,19 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Client._ES.Wallmount.Posters;
+
+// Exists because posters currently just set a single state at the base of spritecomp
+// and because we want directional posters for directionality purposes
+[RegisterComponent]
+public sealed partial class ESPosterComponent : Component
+{
+    // The state to set the poster key to.
+    [DataField(required: true)]
+    public string State = default!;
+}
+
+[Serializable, NetSerializable]
+public enum ESPosterVisuals : byte
+{
+    Base
+}
