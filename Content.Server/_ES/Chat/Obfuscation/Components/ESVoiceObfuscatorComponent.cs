49cba0537414c77c67fd74c574184e3a2414ab7c
EmoGarbage404 <retron404@gmail.com>
2025-11-25 21:56:19 -0500

    Add voice obfuscation

---
+++ Content.Server/_ES/Chat/Obfuscation/Components/ESVoiceObfuscatorComponent.cs
@@ -0,0 +1,16 @@
+using Content.Shared.Speech;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._ES.Chat.Obfuscation.Components;
+
+/// <summary>
+/// This is used for clothing items which hide the name of someone speaking.
+/// This version replaces it with a generic descriptor of their appearance.
+/// </summary>
+[RegisterComponent]
+[Access(typeof(ESVoiceObfuscatorSystem))]
+public sealed partial class ESVoiceObfuscatorComponent : Component
+{
+    [DataField]
+    public ProtoId<SpeechVerbPrototype> SpeechVerb = "DefaultMumble";
+}
