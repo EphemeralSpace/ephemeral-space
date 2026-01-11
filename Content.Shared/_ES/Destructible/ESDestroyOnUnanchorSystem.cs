deabf332a0738968cd09297438d04e4c7999830f
EmoGarbage404 <retron404@gmail.com>
2026-01-11 14:28:13 -0500

    make things break on unanchored instead of lingering around

---
+++ Content.Shared/_ES/Destructible/ESDestroyOnUnanchorSystem.cs
@@ -0,0 +1,23 @@
+using Content.Shared._ES.Destructible.Components;
+using Content.Shared.Destructible;
+
+namespace Content.Shared._ES.Destructible;
+
+public sealed class ESDestroyOnUnanchorSystem : EntitySystem
+{
+    [Dependency] private readonly SharedDestructibleSystem _destructible = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ESDestroyOnUnanchorComponent, AnchorStateChangedEvent>(OnAnchorStateChanged);
+    }
+
+    private void OnAnchorStateChanged(Entity<ESDestroyOnUnanchorComponent> ent, ref AnchorStateChangedEvent args)
+    {
+        if (args.Anchored || args.Detaching)
+            return;
+
+        _destructible.DestroyEntity(ent.Owner);
+    }
+}
