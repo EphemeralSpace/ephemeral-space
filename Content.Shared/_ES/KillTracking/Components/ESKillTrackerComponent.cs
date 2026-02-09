7867aac24c467a83743400220e6d0028fe415f06
EmoGarbage404 <retron404@gmail.com>
2026-02-09 17:06:14 -0500

    Develop kill tracking mildly

--- Content.Shared/_ES/KillTracking/Components/ESKillTrackerComponent.cs
+++ Content.Shared/_ES/KillTracking/Components/ESKillTrackerComponent.cs
@@ -13,27 +13,33 @@ public sealed partial class ESKillTrackerComponent : Component
 public sealed partial class ESDamageSource
 {
     [DataField]
-    public NetEntity? Source;
+    public EntityUid? Source;
 
     [DataField]
     public FixedPoint2 AccumulatedDamage = FixedPoint2.Zero;
 
     public bool IsEnvironment => !Source.HasValue;
+
+    public ESDamageSource(EntityUid? source, FixedPoint2 damage)
+    {
+        Source = source;
+        AccumulatedDamage = damage;
+    }
 }
 
-public static class ESKillTrackerHelpers
+/// <summary>
+/// Event raised on an entity with <see cref="ESKillTrackerComponent"/> when they die.
+/// </summary>
+[ByRefEvent]
+public readonly struct ESPlayerKilledEvent(EntityUid killed, EntityUid? killer)
 {
-    public static ESDamageSource CreateKillTrackingSource(this IEntityManager entMan, FixedPoint2 accumulatedDamage)
-    {
-        return CreateKillTrackingSource(entMan, null, accumulatedDamage);
-    }
+    public readonly EntityUid Killed = killed;
 
-    public static ESDamageSource CreateKillTrackingSource(this IEntityManager entMan, EntityUid? source, FixedPoint2 accumulatedDamage)
-    {
-        return new ESDamageSource
-        {
-            Source = entMan.GetNetEntity(source),
-            AccumulatedDamage = accumulatedDamage,
-        };
-    }
+    public readonly EntityUid? Killer = killer;
+
+    public bool IsValidKill => !(IsSuicide || IsEnvironment);
+
+    public bool IsSuicide => Killed == Killer;
+
+    public bool IsEnvironment => !Killer.HasValue;
 }
--- Content.Shared/_ES/KillTracking/ESKillTrackingSystem.cs
+++ Content.Shared/_ES/KillTracking/ESKillTrackingSystem.cs
@@ -3,7 +3,9 @@ using Content.Shared._ES.KillTracking.Components;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Systems;
 using Content.Shared.FixedPoint;
+using Content.Shared.Mobs;
 using Content.Shared.Mobs.Systems;
+using Content.Shared.Rejuvenate;
 using Robust.Shared.Collections;
 
 namespace Content.Shared._ES.KillTracking;
@@ -14,6 +16,8 @@ public sealed class ESKillTrackingSystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<ESKillTrackerComponent, DamageChangedEvent>(OnDamageChanged, before: [ typeof(MobThresholdSystem) ]);
+        SubscribeLocalEvent<ESKillTrackerComponent, RejuvenateEvent>(OnRejuvenate);
+        SubscribeLocalEvent<ESKillTrackerComponent, MobStateChangedEvent>(OnMobStateChanged);
     }
 
     private void OnDamageChanged(Entity<ESKillTrackerComponent> ent, ref DamageChangedEvent args)
@@ -22,29 +26,54 @@ public sealed class ESKillTrackingSystem : EntitySystem
         if (args.DamageDelta is not { } delta)
             return;
 
-        ReduceDamage(ent.AsNullable(), DamageSpecifier.GetNegative(delta).GetTotal());
-        AddDamage(ent.AsNullable(), args.Origin, DamageSpecifier.GetPositive(delta).GetTotal());
+        ReduceDamage(ent, DamageSpecifier.GetNegative(delta).GetTotal());
+        AddDamage(ent, args.Origin, DamageSpecifier.GetPositive(delta).GetTotal());
     }
 
-    public void AddDamage(Entity<ESKillTrackerComponent?> ent, EntityUid? source, FixedPoint2 damage)
+    private void OnRejuvenate(Entity<ESKillTrackerComponent> ent, ref RejuvenateEvent args)
     {
-        if (!Resolve(ent, ref ent.Comp))
+        ent.Comp.Sources.Clear();
+    }
+
+    private void OnMobStateChanged(Entity<ESKillTrackerComponent> ent, ref MobStateChangedEvent args)
+    {
+        // Only report on dead.
+        if (args.NewMobState != MobState.Dead)
             return;
 
-        if (ent.Comp.Sources.FirstOrDefault(e => GetEntity(e.Source) == source) is { } elem)
+        var killer = ent.Comp.Sources.Count switch
+        {
+            > 1 => ent.Comp.Sources.Where(s => !s.IsEnvironment).MaxBy(s => s.AccumulatedDamage)?.Source,
+            1 => ent.Comp.Sources.First().Source,
+            _ => null,
+        };
+
+        // TODO: Testing logs.
+        Log.Debug($"{ToPrettyString(ent)} was killed by {ToPrettyString(killer)}!");
+
+        var ev = new ESPlayerKilledEvent(ent, killer);
+        RaiseLocalEvent(ent, ref ev);
+    }
+
+    private void AddDamage(Entity<ESKillTrackerComponent> ent, EntityUid? source, FixedPoint2 damage)
+    {
+        if (ent.Comp.Sources.FirstOrDefault(e => e.Source == source) is { } elem)
         {
             elem.AccumulatedDamage += damage;
         }
         else
         {
-            ent.Comp.Sources.Add(EntityManager.CreateKillTrackingSource(source, damage));
+            ent.Comp.Sources.Add(new ESDamageSource(source, damage));
+        }
+
+        if (source.HasValue)
+        {
+            // TODO: Relation tracking stuff goes here.
         }
     }
 
-    public void ReduceDamage(Entity<ESKillTrackerComponent?> ent, FixedPoint2 damage)
+    private void ReduceDamage(Entity<ESKillTrackerComponent> ent, FixedPoint2 damage)
     {
-        if (!Resolve(ent, ref ent.Comp))
-            return;
         var toRemove = new ValueList<ESDamageSource>();
         foreach (var source in ent.Comp.Sources)
         {
