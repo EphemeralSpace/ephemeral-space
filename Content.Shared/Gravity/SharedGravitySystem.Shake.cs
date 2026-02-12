592f3b87d5adddc26f055c6da9125ca256022b5f
mirrorcult <mirrorcult@pm.me>
2026-02-12 13:53:21 -0700

    grav shake

--- Content.Shared/Gravity/SharedGravitySystem.Shake.cs
+++ Content.Shared/Gravity/SharedGravitySystem.Shake.cs
@@ -1,4 +1,6 @@
+using Content.Shared._ES.Camera;
 using Content.Shared.GameTicking;
+using Robust.Shared.Player;
 using Robust.Shared.Timing;
 
 namespace Content.Shared.Gravity;
@@ -7,6 +9,7 @@ public abstract partial class SharedGravitySystem
 {
     // ES START
     [Dependency] private readonly SharedGameTicker _ticker = default!;
+    [Dependency] private readonly ESScreenshakeSystem _shake = default!;
     // ES END
 
     protected const float GravityKick = 100.0f;
@@ -49,8 +52,15 @@ public abstract partial class SharedGravitySystem
         // i did not want to have to think this logic through more. this is the simplest solution i could think of
         if (Timing.CurTime - _ticker.RoundStartTimeSpan < TimeSpan.FromSeconds(30))
             return;
-        // ES END
 
+        // ES SCREENSHAKE LOGIC
+        // instead of poopass camera kick
+        var translation = new ESScreenshakeParameters() { Trauma = 0.8f, DecayRate = 0.04f, Frequency = 0.015f };
+        var filter = Filter.BroadcastGrid(uid);
+        _shake.Screenshake(filter, translation, null);
+
+        return;
+        // ES END
         if (!TryComp<GravityShakeComponent>(uid, out var shake))
         {
             shake = AddComp<GravityShakeComponent>(uid);
