9a35458803db616092d8e59176057370359d998f
EmoGarbage404 <retron404@gmail.com>
2026-02-21 01:22:21 -0500

    Add processor and repair functionality

--- Content.Shared/_ES/Radio/Components/ESRadioScramblerComponent.cs
+++ Content.Shared/_ES/Radio/Components/ESRadioScramblerComponent.cs
@@ -1,4 +1,6 @@
+using Content.Shared.DoAfter;
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._ES.Radio.Components;
 
@@ -7,4 +9,16 @@ public sealed partial class ESRadioScramblerComponent : Component
 {
     [DataField, AutoNetworkedField]
     public bool Hacked;
+
+    [DataField]
+    public TimeSpan RepairDelay = TimeSpan.FromSeconds(15);
 }
+
+[Serializable, NetSerializable]
+public enum ESRadioScramblerVisuals : byte
+{
+    Hacked,
+}
+
+[Serializable, NetSerializable]
+public sealed partial class ESRepairRadioScramblerDoAfterEvent : SimpleDoAfterEvent;
