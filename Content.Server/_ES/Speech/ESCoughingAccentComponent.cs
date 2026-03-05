e407404b373a33718ff2fdc1c44a0367c6e41846
mirrorcult <mirrorcult@pm.me>
2026-03-05 02:56:06 -0700

    more idk

--- Content.Server/_ES/Speech/ESCoughingAccentComponent.cs
+++ Content.Server/_ES/Speech/ESCoughingAccentComponent.cs
@@ -13,16 +13,16 @@ public sealed partial class ESCoughingAccentComponent : Component
     ///     Chance, per interjection that rolls, that the message will be cut off, ending there.
     /// </summary>
     [DataField]
-    public float CutOffMessageChancePerInterjection = 1f / 4f;
+    public float CutOffMessageChancePerInterjection = 1f / 5f;
 
     [DataField]
-    public float InterjectionChancePerCharacter = 0.07f;
+    public float InterjectionChancePerCharacter = 0.09f;
 
     [DataField]
     public float MinMessageLength = 4;
 
     [DataField]
-    public float InterjectionCapitalizeChance = 1f / 3f;
+    public float InterjectionCapitalizeChance = 1f / 2f;
 
     [DataField]
     public ProtoId<LocalizedDatasetPrototype> CoughingInterjectionDataset = "ESCoughingInterjections";
