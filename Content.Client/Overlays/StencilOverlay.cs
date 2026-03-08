9c283885e3a9e468c601367f0ac5f3a544da2fc8
Red <96445749+TheShuEd@users.noreply.github.com>
2026-03-09 02:28:48 +0300

    Weather entities (#41427)
    * weather status effect entities
    
    * proto port + fixes
    
    * Update weather.yml
    
    * fix visuals
    
    * Update SharedWeatherSystem.cs
    
    * Update SharedWeatherSystem.cs
    
    * Update WeatherSystem.cs
    
    * Thanks Slarti, thats a much better
    
    * Update WeatherSystem.cs
    
    * Update StencilOverlay.Weather.cs
    
    * Update Content.Client/Overlays/StencilOverlay.Weather.cs
    
    Co-authored-by: Pok <113675512+Pok27@users.noreply.github.com>
    
    * Update Content.Shared/StatusEffectNew/Components/StatusEffectComponent.cs
    
    Co-authored-by: Pok <113675512+Pok27@users.noreply.github.com>
    
    * Update Content.Client/Overlays/StencilOverlay.Weather.cs
    
    Co-authored-by: Pok <113675512+Pok27@users.noreply.github.com>
    
    * Revise weather command help descriptions
    
    Updated weather command help texts for clarity.
    
    * Merge branch 'master' into ed-14-11-2025-weather-entities
    
    * Tayrtahn review apply
    
    * fixes and cleanup
    
    ---------
    
    Co-authored-by: Pok <113675512+Pok27@users.noreply.github.com>
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Client/Overlays/StencilOverlay.cs
+++ Content.Client/Overlays/StencilOverlay.cs
@@ -2,11 +2,12 @@ using System.Numerics;
 using Content.Client.Graphics;
 using Content.Client.Parallax;
 using Content.Client.Weather;
+using Content.Shared.StatusEffectNew;
+using Content.Shared.StatusEffectNew.Components;
 using Content.Shared.Weather;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
 using Robust.Shared.Enums;
-using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 
@@ -31,6 +32,8 @@ public sealed partial class StencilOverlay : Overlay
     private readonly SharedMapSystem _map;
     private readonly SpriteSystem _sprite;
     private readonly WeatherSystem _weather;
+    private readonly StatusEffectsSystem _statusEffects;
+    private HashSet<Entity<WeatherStatusEffectComponent, StatusEffectComponent>>? _weatherSet = new();
 
     public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
 
@@ -38,7 +41,7 @@ public sealed partial class StencilOverlay : Overlay
 
     private readonly ShaderInstance _shader;
 
-    public StencilOverlay(ParallaxSystem parallax, SharedTransformSystem transform, SharedMapSystem map, SpriteSystem sprite, WeatherSystem weather)
+    public StencilOverlay(ParallaxSystem parallax, SharedTransformSystem transform, SharedMapSystem map, SpriteSystem sprite, WeatherSystem weather, StatusEffectsSystem statusEffects)
     {
         ZIndex = ParallaxSystem.ParallaxZIndex + 1;
         _parallax = parallax;
@@ -46,6 +49,7 @@ public sealed partial class StencilOverlay : Overlay
         _map = map;
         _sprite = sprite;
         _weather = weather;
+        _statusEffects = statusEffects;
         IoCManager.InjectDependencies(this);
         _shader = _protoManager.Index(CircleShader).InstanceUnique();
     }
@@ -63,17 +67,8 @@ public sealed partial class StencilOverlay : Overlay
             res.Blep = _clyde.CreateRenderTarget(args.Viewport.Size, new RenderTargetFormatParameters(RenderTargetColorFormat.Rgba8Srgb), name: "weather-stencil");
         }
 
-        if (_entManager.TryGetComponent<WeatherComponent>(mapUid, out var comp))
-        {
-            foreach (var (proto, weather) in comp.Weather)
-            {
-                if (!_protoManager.Resolve<WeatherPrototype>(proto, out var weatherProto))
-                    continue;
-
-                var alpha = _weather.GetPercent(weather, mapUid);
-                DrawWeather(args, res, weatherProto, alpha, invMatrix);
-            }
-        }
+        if (_statusEffects.TryEffectsWithComp(mapUid, out _weatherSet))
+            DrawWeather(args, res, _weatherSet, invMatrix);
 
         args.WorldHandle.UseShader(null);
         args.WorldHandle.SetTransform(Matrix3x2.Identity);
