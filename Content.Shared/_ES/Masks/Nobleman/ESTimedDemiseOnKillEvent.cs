0ca4d4ecd96fa0df502311c9aeccb960f7b2494b
Ermucat <NCramey09@gmail.com>
2026-03-21 15:15:54 -0400

    a

--- Content.Shared/_ES/Masks/Nobleman/ESTimedDemiseOnKillEvent.cs
+++ Content.Shared/_ES/Masks/Nobleman/ESTimedDemiseOnKillEvent.cs
@@ -1,7 +1,9 @@
 ﻿using Content.Shared._ES.Core.Timer.Components;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._ES.Masks.Nobleman;
 
+[Serializable, NetSerializable]
 public sealed partial class ESTimedDemiseOnKillEvent : ESEntityTimerEvent
 {
 
