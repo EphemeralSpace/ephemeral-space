51d7bfda9b186cfb49d5e209be83d6f2d1c92131
mirrorcult <mirrorcult@pm.me>
2026-03-28 14:07:40 -0700

    actual fix this jsut isnt necessary

--- Content.Client/_ES/Stagehand/Ui/StagehandObserveUIController.cs
+++ Content.Client/_ES/Stagehand/Ui/StagehandObserveUIController.cs
@@ -40,8 +40,6 @@ public sealed class StagehandObserveUIController : UIController, IOnStateEntered
 
     public void OnStateExited(GameplayState state)
     {
-        _mask.OnMaskChanged -= OnMaskChanged;
-        _objective.OnObjectivesChanged -= OnObjectivesChanged;
     }
 
     private void OnMaskChanged(EntityUid mind, ProtoId<ESMaskPrototype>? mask)
