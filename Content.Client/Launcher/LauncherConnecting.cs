1916e73447f3e6596ef02f6d0b29eb8fdf6a0470
mirrorcult <mirrorcult@pm.me>
2026-04-06 04:26:46 -0700

    actual code for making it work

--- Content.Client/Launcher/LauncherConnecting.cs
+++ Content.Client/Launcher/LauncherConnecting.cs
@@ -1,5 +1,6 @@
 using System;
 using Content.Client.Stylesheets;
+using Content.Shared._ES.Tips;
 using Robust.Client;
 using Robust.Client.UserInterface;
 using Robust.Shared.Configuration;
@@ -17,12 +18,10 @@ namespace Content.Client.Launcher
         [Dependency] private readonly IClientNetManager _clientNetManager = default!;
         [Dependency] private readonly IGameController _gameController = default!;
         [Dependency] private readonly IBaseClient _baseClient = default!;
-        [Dependency] private readonly IRobustRandom _random = default!;
-        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-        [Dependency] private readonly IConfigurationManager _cfg = default!;
         [Dependency] private readonly IClipboardManager _clipboard = default!;
         [Dependency] private readonly IStylesheetManager _stylesheets = default!;
         [Dependency] private readonly ILogManager _logManager = default!;
+        [Dependency] private readonly ESTipsManager _tips = default!;
 
         private LauncherConnectingGui? _control;
         private ISawmill _sawmill = default!;
@@ -63,7 +62,7 @@ namespace Content.Client.Launcher
 
         protected override void Startup()
         {
-            _control = new LauncherConnectingGui(this, _random, _prototypeManager, _cfg, _clipboard, _stylesheets);
+            _control = new LauncherConnectingGui(this, _clipboard, _stylesheets, _tips);
 
             _sawmill = _logManager.GetSawmill("launcher-ui");
 
