6cb67482613cae1699720a157275b34b55bae463
Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
2026-04-08 10:47:18 -0400

    Make non-human jobs work correctly (#1713)
    * Make job entities work correctly
    
    * Fix error

--- Content.Client/_ES/Spawning/Ui/ESJobDisplay.xaml.cs
+++ Content.Client/_ES/Spawning/Ui/ESJobDisplay.xaml.cs
@@ -55,7 +55,9 @@ public sealed partial class ESJobDisplay : BoxContainer
             : Loc.GetString(jobProto.Description));
 
         _entityManager.DeleteEntity(_doll);
-        _doll = _entityManager.Spawn(_prototypeManager.Index(SharedHumanoidAppearanceSystem.DefaultSpecies).DollPrototype);
+        _doll = jobProto.JobEntity is null
+            ? _entityManager.Spawn(_prototypeManager.Index(SharedHumanoidAppearanceSystem.DefaultSpecies).DollPrototype)
+            : _entityManager.Spawn(jobProto.JobEntity);
         var randomSeed = new RngSeed().SeedForStep(job.Id.GetHashCode() + _player.LocalEntity?.Id ?? 0);
         var defaultDude = _auditions.RandomProfile(randomSeed.IntoRandomizer());
         _humanoid.LoadProfile(_doll, defaultDude);
