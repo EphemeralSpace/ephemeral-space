5cd3160490b35200b5c60632d45cdc0a0e7cacec
EmoGarbage404 <retron404@gmail.com>
2026-04-13 22:01:33 -0400

    Increase average radstorm time

--- Content.Server/_ES/Radstorm/ESRadstormRoundEndRuleComponent.cs
+++ Content.Server/_ES/Radstorm/ESRadstormRoundEndRuleComponent.cs
@@ -16,7 +16,7 @@ public sealed partial class ESRadstormRoundEndRuleComponent : Component
     ///     Average time that the radstorm can start at. Used when randomly picking <see cref="RadstormTimeRemaining"/>.
     /// </summary>
     [DataField]
-    public TimeSpan RadstormStartTimeAvg = TimeSpan.FromMinutes(60f);
+    public TimeSpan RadstormStartTimeAvg = TimeSpan.FromMinutes(70f);
 
     /// <summary>
     ///     Standard deviation for time that the radstorm can start at. Used when randomly picking <see cref="RadstormTimeRemaining"/>.
