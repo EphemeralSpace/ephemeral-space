f93d993b41a8b43e60bfa1c080ff590d0035bf7c
Flareguy <woaj9999@outlook.com>
2026-04-17 18:53:46 -0500

    remove atmos ambience

--- Content.Shared/_ES/Audio/AmbientMusic/ESAmbientMusicMarkerAtmosComponent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._ES.Audio.AmbientMusic;
-
-/// <summary>
-///     Marks an entity as contributing to Atmospherics ambient music.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class ESAmbientMusicMarkerAtmosComponent : Component;
