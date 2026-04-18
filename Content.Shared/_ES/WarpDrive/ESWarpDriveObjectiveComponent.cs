3ab0cf75c4f2b671285449e009e45439567c2e4d
mirrorcult <mirrorcult@pm.me>
2026-04-18 06:16:03 -0700

    move you

--- Content.Shared/_ES/WarpDrive/ESWarpDriveObjectiveComponent.cs
+++ Content.Shared/_ES/WarpDrive/ESWarpDriveObjectiveComponent.cs
@@ -1,10 +1,9 @@
 using Robust.Shared.GameStates;
 
-namespace Content.Shared._ES.Telesci.Components;
+namespace Content.Shared._ES.WarpDrive;
 
 /// <summary>
 /// Basic objective that progresses based on whether the warp drive has charged or not.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-[Access(typeof(ESSharedTelesciSystem))]
 public sealed partial class ESWarpDriveObjectiveComponent : Component;
