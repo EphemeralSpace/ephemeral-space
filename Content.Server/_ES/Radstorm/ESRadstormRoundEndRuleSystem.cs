6254eb0912141a10d40d3b6072a84b5b1e269992
mazzie <mazziechai@gmail.com>
2026-05-23 14:10:05 -0700

    feat: force end round when all players are dead (#1860)
    commit the correct code, remove unnecessary death check in radstorm dmg

--- Content.Server/_ES/Radstorm/ESRadstormRoundEndRuleSystem.cs
+++ Content.Server/_ES/Radstorm/ESRadstormRoundEndRuleSystem.cs
@@ -90,8 +90,6 @@ public sealed partial class ESRadstormRoundEndRuleSystem : GameRuleSystem<ESRads
         {
             component.RadstormNextDamageTickTime = _timing.CurTime + TimeSpan.FromSeconds(1);
 
-            // HELL EVERLASTING! DIE FOREVER!
-            var stillAlive = 0;
             // this should probably not be bounded to mobstate and instead be its own thing but whatever
             var killQuery = EntityQueryEnumerator<MobStateComponent, DamageableComponent, TransformComponent>();
             while (killQuery.MoveNext(out var mob, out var state, out var damageable, out var xform))
@@ -110,18 +108,22 @@ public sealed partial class ESRadstormRoundEndRuleSystem : GameRuleSystem<ESRads
                 if (TryComp<BrainDamageComponent>(mob, out var brainDamage))
                     _brainDamage.TryChangeBrainDamage((mob, brainDamage), brainDamage.MaxDamage / 20);
 
-                // only count mobs which actually end up taking damage from this
-                var dmg = _damage.ChangeDamage((mob, damageable), component.RadstormDamagePerSecond, true, false);
-                if (dmg.GetTotal() > FixedPoint2.Zero && state.CurrentState != MobState.Dead)
-                    stillAlive += 1;
+                _damage.ChangeDamage((mob, damageable), component.RadstormDamagePerSecond, true, false);
             }
+        }
 
-            // show is over
-            // (make sure we only actually do this if after time and not just deadly space)
-            // (i kind of implemented that in a weird way huh)
-            if (stillAlive == 0 && RadstormStarted((uid, component)))
-                _roundEnd.EndRound();
+        // If everyone's dead, end the round
+        var actorQuery = EntityQueryEnumerator<ActorComponent>();
+        var allDead = true;
+        while (actorQuery.MoveNext(out var mob, out _))
+        {
+            if (TryComp<MobStateComponent>(mob, out var state) && state.CurrentState != MobState.Dead)
+                allDead = false;
+        }
 
+        if (allDead)
+        {
+            _roundEnd.EndRound();
             return;
         }
 
