9dfdeda08640b77ed0f82f310e775d9ccd2d2def
EmoGarbage404 <retron404@gmail.com>
2026-05-25 21:28:53 -0400

    First commit

---
+++ Content.Shared/_ES/Masks/Traitor/Components/ESTraitorBugObjectiveComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._ES.Masks.Traitor.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ESTraitorBugObjectiveComponent : Component
+{
+
+}
