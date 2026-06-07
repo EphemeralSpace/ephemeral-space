4170ec20931c707347699a08757fa31d8278775d
EmoGarbage404 <retron404@gmail.com>
2026-06-07 13:34:32 -0400

    Admin clothing

--- Content.Server/_ES/Stagehand/Components/ESUsernameEntityNameComponent.cs
+++ Content.Server/_ES/Stagehand/Components/ESUsernameEntityNameComponent.cs
@@ -1,10 +1,8 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._ES.Stagehand.Components;
+namespace Content.Server._ES.Stagehand.Components;
 
 /// <summary>
 /// Used for entities which show the player's username when they are occupied, rather than an IC name.
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent]
 [Access(typeof(ESUsernameEntityNameSystem))]
 public sealed partial class ESUsernameEntityNameComponent : Component;
