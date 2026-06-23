710875d3cc9a2e1d2d176a3af2dcbd3bad95491e
mirrorcult <mirrorcult@pm.me>
2026-06-22 17:58:35 -0700

    fixes

--- Content.Shared/_ES/Light/Components/ESTileBasedRoofComponent.cs
+++ Content.Shared/_ES/Light/Components/ESTileBasedRoofComponent.cs
@@ -18,7 +18,7 @@ public sealed partial class ESTileBasedRoofComponent : Component
     public HashSet<ProtoId<ContentTileDefinition>> UnRoofedTiles = new()
     {
         "Lattice", // Space structures
-        "ESDamagedLattice",
+        "ESLatticeDamaged",
         "FloorGlass", // See-through tiles
         "FloorRGlass",
         "FloorAsteroidSand", // natural non-station tiles
