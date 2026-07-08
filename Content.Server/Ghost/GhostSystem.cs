394b997d3017c965a5d66ed34b54266061b1a42e
mirrorcult <mirrorcult@pm.me>
2026-07-08 11:49:39 -0700

    oop

--- Content.Server/Ghost/GhostSystem.cs
+++ Content.Server/Ghost/GhostSystem.cs
@@ -23,8 +23,11 @@ using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Movement.Events;
+using Content.Shared.Movement.Systems;
+using Content.Shared.NameModifier.EntitySystems;
 using Content.Shared.Popups;
 using Content.Shared.Storage.Components;
+using Content.Shared.Tag;
 using Content.Shared.Warps;
 using Robust.Server.GameObjects;
 using Robust.Shared.Configuration;
@@ -66,6 +69,7 @@ namespace Content.Server.Ghost
         [Dependency] private DamageableSystem _damageable = default!;
         [Dependency] private SharedPopupSystem _popup = default!;
         [Dependency] private IRobustRandom _random = default!;
+        [Dependency] private NameModifierSystem _nameMod = default!;
 // ES START
         [Dependency] private ESStagehandSystem _stagehand = default!;
 // ES END
@@ -495,6 +499,9 @@ namespace Content.Server.Ghost
                 _minds.TransferTo(mind.Owner, ghost, mind: mind.Comp);
             Log.Debug($"Spawned ghost \"{ToPrettyString(ghost)}\" for {mind.Comp.CharacterName}.");
 
+            // we changed the entity name above
+            // we have to call this after the mind has been transferred since some mind roles modify the ghost's name
+            _nameMod.RefreshNameModifiers(ghost);
             return ghost;
         }
 
