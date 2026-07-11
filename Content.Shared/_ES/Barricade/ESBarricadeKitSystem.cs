5be54315cf3e21deedc20eecaee51e64101f93ff
mirrorcult <mirrorcult@pm.me>
2026-07-11 10:24:01 -0700

    support fulltile barricades when used inhand

--- Content.Shared/_ES/Barricade/ESBarricadeKitSystem.cs
+++ Content.Shared/_ES/Barricade/ESBarricadeKitSystem.cs
@@ -1,9 +1,11 @@
 using Content.Shared._ES.Barricade.Components;
 using Content.Shared.Charges.Systems;
+using Content.Shared.Coordinates.Helpers;
 using Content.Shared.DoAfter;
 using Content.Shared.Doors.Components;
 using Content.Shared.Doors.Systems;
 using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
 
 namespace Content.Shared._ES.Barricade;
 
@@ -17,9 +19,18 @@ public sealed partial class ESBarricadeKitSystem : EntitySystem
     /// <inheritdoc/>
     public override void Initialize()
     {
+        SubscribeLocalEvent<ESBarricadeKitComponent, UseInHandEvent>(OnUseInHand);
         SubscribeLocalEvent<ESBarricadeKitComponent, AfterInteractEvent>(OnAfterInteract);
-        SubscribeLocalEvent<ESBarricadeKitComponent, ESBarricadeAirlockDoAfterEvent>(OnBarricadeAirlockDoAfter);
-        SubscribeLocalEvent<ESBarricadeKitComponent, DoAfterAttemptEvent<ESBarricadeAirlockDoAfterEvent>>(OnBarricadeAirlockDoAfterAttempt);
+        SubscribeLocalEvent<ESBarricadeKitComponent, ESBarricadeDoAfterEvent>(OnBarricadeDoAfter);
+        SubscribeLocalEvent<ESBarricadeKitComponent, DoAfterAttemptEvent<ESBarricadeDoAfterEvent>>(OnBarricadeDoAfterAttempt);
+    }
+
+    private void OnUseInHand(Entity<ESBarricadeKitComponent> ent, ref UseInHandEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = StartDoafter(ent, ESBarricadeKitBarricadeType.Tile, args.User, null);
     }
 
     private void OnAfterInteract(Entity<ESBarricadeKitComponent> ent, ref AfterInteractEvent args)
@@ -27,14 +38,20 @@ public sealed partial class ESBarricadeKitSystem : EntitySystem
         if (args.Handled)
             return;
 
-        if (args.Target.HasValue && CanBarricade(args.Target.Value))
-        {
-            args.Handled = _doAfter.TryStartDoAfter(new DoAfterArgs(EntityManager,
-                args.User,
+        args.Handled = StartDoafter(ent, ESBarricadeKitBarricadeType.Airlock, args.User, args.Target);
+    }
+
+    private bool StartDoafter(Entity<ESBarricadeKitComponent> ent, ESBarricadeKitBarricadeType type, EntityUid user, EntityUid? target)
+    {
+        if (target.HasValue && !CanBarricade(target.Value))
+            return false;
+
+        return _doAfter.TryStartDoAfter(new DoAfterArgs(EntityManager,
+                user,
                 ent.Comp.SetupDelay,
-                new ESBarricadeAirlockDoAfterEvent(),
+                new ESBarricadeDoAfterEvent() { Type = type },
                 ent,
-                args.Target.Value,
+                target ?? user,
                 ent)
             {
                 BreakOnDamage = true,
@@ -43,22 +60,25 @@ public sealed partial class ESBarricadeKitSystem : EntitySystem
                 DuplicateCondition = DuplicateConditions.SameTool,
                 AttemptFrequency = AttemptFrequency.EveryTick,
             });
-        }
     }
 
-    private void OnBarricadeAirlockDoAfter(Entity<ESBarricadeKitComponent> ent, ref ESBarricadeAirlockDoAfterEvent args)
+    private void OnBarricadeDoAfter(Entity<ESBarricadeKitComponent> ent, ref ESBarricadeDoAfterEvent args)
     {
         if (args.Handled || args.Cancelled || args.Target is not { } target)
             return;
 
-        PredictedSpawnAtPosition(ent.Comp.AirlockBarricade, Transform(target).Coordinates);
+        var proto = args.Type == ESBarricadeKitBarricadeType.Airlock
+            ? ent.Comp.AirlockBarricade
+            : ent.Comp.TileBarricade;
+
+        PredictedSpawnAtPosition(proto, Transform(target).Coordinates.SnapToGrid(entMan: EntityManager));
         _charge.TryUseCharge(ent.Owner);
         args.Handled = true;
     }
 
-    private void OnBarricadeAirlockDoAfterAttempt(Entity<ESBarricadeKitComponent> ent, ref DoAfterAttemptEvent<ESBarricadeAirlockDoAfterEvent> args)
+    private void OnBarricadeDoAfterAttempt(Entity<ESBarricadeKitComponent> ent, ref DoAfterAttemptEvent<ESBarricadeDoAfterEvent> args)
     {
-        if (args.Event.Target is null || !CanBarricade(args.Event.Target.Value))
+        if (args.Event.Type == ESBarricadeKitBarricadeType.Airlock && (args.Event.Target is null || !CanBarricade(args.Event.Target.Value)))
             args.Cancel();
     }
 
