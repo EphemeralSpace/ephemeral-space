f13e34abacea4de2d572ea70891cd62a4ff60b99
mirrorcult <mirrorcult@pm.me>
2026-07-13 11:15:26 -0700

    fix buckle interaction particles

--- Content.Shared/Interaction/SharedInteractionSystem.cs
+++ Content.Shared/Interaction/SharedInteractionSystem.cs
@@ -561,7 +561,7 @@ namespace Content.Shared.Interaction
             RaiseLocalEvent(user, userMessage, true);
 
             _adminLogger.Add(LogType.InteractHand, LogImpact.Low, $"{user} interacted with {target}");
-            DoContactInteraction(user, target, null, true, message); // Stellar - interaction particles
+            DoContactInteraction(user, target, null, true, message, interactionParticles: message.InteractionParticle); // Stellar - interaction particles
             if (message.Handled || userMessage.Handled)
                 return;
 
