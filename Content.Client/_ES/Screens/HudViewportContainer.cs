cb111166f4e2114bcca84387fd78e1c9b19a6941
mirrorcult <19853115+mirrorcult@users.noreply.github.com>
2026-07-16 14:26:16 +0000

    fix viewport layouting for good (#2142)
    * what the hell am i even doing
    
    * do i even need to measure shit
    
    * i dont think i needed to do any of that shit maybe
    
    * dumb ass
    
    * better snapping probably and cleanup
    
    * aoughhhh this shit is loike nonfunctional
    
    * aoughhhh
    
    * hold on im cooking
    
    * i dont knowww i dont knowwww
    
    * DUNCE!!!!!!!!!!!!!
    
    * check overfill as well for 1366x768 enjoyers
    
    * a
    
    * fix panel width not using passed in size, always offset by left panel width even if its invisible
    
    * uhh dewfault higher cause it still crashes word wrapping sometimes
    
    ---------
    
    Co-authored-by: mirrorcult <mirrorcult@pm.me>

--- Content.Client/_ES/Screens/HudViewportContainer.cs
+++ Content.Client/_ES/Screens/HudViewportContainer.cs
@@ -1,13 +1,15 @@
 using System.Numerics;
-using Content.Client.UserInterface.Controls;
+using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Timing;
 
 namespace Content.Client._ES.Screens;
 
 /// <summary>
 ///     Handles layouting for the hud UI with a left panel, viewport, and right panel.
 ///     All children will fill vertical space as much as possible.
-///     The viewport will be laid out first
+///     The viewport will be laid out first.
+///     The left and right panels may toggle visibility if their calculated size is too small.
 /// </summary>
 public sealed class HudViewportContainer : Container
 {
@@ -19,27 +21,82 @@ public sealed class HudViewportContainer : Container
     /// </summary>
     public float PanelRatio { get; set; } = 1.0f;
 
-    protected override Vector2 ArrangeOverride(Vector2 finalSize)
+    /// <summary>
+    ///     A panel will be hidden entirely if its calculated pixel size would be below this value
+    /// </summary>
+    public float HidePanelsBelow { get; set; } = 180.0f;
+
+    /// <summary>
+    ///     A panel will be made visible (assuming its not already visible) if its calculated pixel size would be above this value
+    /// </summary>
+    public float ShowPanelsAbove { get; set; } = 200.0f;
+
+    private bool _leftVisible = false;
+    private bool _rightVisible = false;
+
+    private bool ShouldShowPanel(Control panel, float availableWidth)
+    {
+        if (panel.Visible)
+            return (availableWidth * UIScale) >= HidePanelsBelow;
+        return (availableWidth * UIScale) >= ShowPanelsAbove;
+    }
+
+    private (float left, float right) CalculatePanelWidths(float totalWidth, float viewportWidth)
+    {
+        var panelSpace = Math.Max(0f, totalWidth - viewportWidth);
+
+        var rightPanelWidth = panelSpace / (PanelRatio + 1);
+        var leftPanelWidth = panelSpace - rightPanelWidth;
+
+        return (leftPanelWidth, rightPanelWidth);
+    }
+
+    protected override Vector2 MeasureOverride(Vector2 availableSize)
     {
         if (ChildCount != 3)
             throw new ArgumentOutOfRangeException($"Child count of {nameof(HudViewportContainer)} must be exactly 3");
-
-        var (finalWidth, finalHeight) = finalSize;
 
         var leftPanel = GetChild(0);
         var centerContainer = GetChild(1);
         var rightPanel = GetChild(2);
 
-        // lay out viewport in the center
+        centerContainer.Measure(availableSize);
+        var (leftPanelWidth, rightPanelWidth) = CalculatePanelWidths(availableSize.X, centerContainer.DesiredSize.X);
+        _leftVisible = ShouldShowPanel(leftPanel, leftPanelWidth);
+        _rightVisible = ShouldShowPanel(rightPanel, rightPanelWidth);
+        leftPanel.Visible = _leftVisible;
+        rightPanel.Visible = _rightVisible;
+
+        if (_leftVisible)
+            leftPanel.Measure(availableSize with { X = leftPanelWidth });
+        if (_rightVisible)
+            rightPanel.Measure(availableSize with { X = rightPanelWidth });
+
+        return availableSize;
+    }
+
+    protected override Vector2 ArrangeOverride(Vector2 finalSize)
+    {
+        if (ChildCount != 3)
+            throw new ArgumentOutOfRangeException($"Child count of {nameof(HudViewportContainer)} must be exactly 3");
+
+        var finalHeight = finalSize.Y;
+
+        var leftPanel = GetChild(0);
+        var centerContainer = GetChild(1);
+        var rightPanel = GetChild(2);
+
         var viewportWidth = centerContainer.DesiredSize.X;
-        var panelSpace = (finalWidth - viewportWidth);
-        var rightPanelWidth = panelSpace / (PanelRatio + 1);
-        var leftPanelWidth = panelSpace - rightPanelWidth;
+        var (leftPanelWidth, rightPanelWidth) = CalculatePanelWidths(finalSize.X, viewportWidth);
+
+        // arrange viewport first
         centerContainer.Arrange(UIBox2.FromDimensions(leftPanelWidth, 0, viewportWidth, finalHeight));
 
-        // lay out panels
-        leftPanel.Arrange(UIBox2.FromDimensions(0, 0, leftPanelWidth, finalHeight));
-        rightPanel.Arrange(UIBox2.FromDimensions(leftPanelWidth + viewportWidth, 0, rightPanelWidth, finalHeight));
+        // arrange panels around it
+        if (_leftVisible)
+            leftPanel.Arrange(UIBox2.FromDimensions(0, 0, leftPanelWidth, finalHeight));
+        if (_rightVisible)
+            rightPanel.Arrange(UIBox2.FromDimensions(leftPanelWidth + viewportWidth, 0, rightPanelWidth, finalHeight));
 
         return finalSize;
     }
