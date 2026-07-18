cc8849482652eaaf4f05f25eee1230d7df7e8d5c
mirrorcult <19853115+mirrorcult@users.noreply.github.com>
2026-07-18 14:11:23 -0700

    remove nuke anchor button

--- Content.Server/Nuke/NukeSystem.cs
+++ Content.Server/Nuke/NukeSystem.cs
@@ -67,7 +67,6 @@ public sealed partial class NukeSystem : EntitySystem
         SubscribeLocalEvent<NukeComponent, AnchorStateChangedEvent>(OnAnchorChanged);
 
         // ui events
-        SubscribeLocalEvent<NukeComponent, NukeAnchorMessage>(OnAnchorButtonPressed);
         SubscribeLocalEvent<NukeComponent, NukeArmedMessage>(OnArmButtonPressed);
         SubscribeLocalEvent<NukeComponent, NukeKeypadMessage>(OnKeypadButtonPressed);
         SubscribeLocalEvent<NukeComponent, NukeKeypadClearMessage>(OnClearButtonPressed);
@@ -173,57 +172,6 @@ public sealed partial class NukeSystem : EntitySystem
     #endregion
 
     #region UI Events
-
-    private async void OnAnchorButtonPressed(EntityUid uid, NukeComponent component, NukeAnchorMessage args)
-    {
-        // malicious client sanity check
-        if (component.Status == NukeStatus.ARMED)
-            return;
-
-        // Nuke has to have the disk in it to be moved
-        if (!component.DiskSlot.HasItem)
-        {
-            var msg = Loc.GetString("nuke-component-cant-anchor-toggle");
-            _popups.PopupEntity(msg, uid, args.Actor, PopupType.MediumCaution);
-            return;
-        }
-
-        // manually set transform anchor (bypassing anchorable)
-        // todo: it will break pullable system
-        var xform = Transform(uid);
-        if (xform.Anchored)
-        {
-            _transform.Unanchor(uid, xform);
-            _itemSlots.SetLock(uid, component.DiskSlot, true);
-        }
-        else
-        {
-/* ES START
-            // Disable the anchoring check for nukes
-            if (!TryComp<MapGridComponent>(xform.GridUid, out var grid))
-                return;
-
-            var worldPos = _transform.GetWorldPosition(xform);
-
-            foreach (var tile in _map.GetTilesIntersecting(xform.GridUid.Value, grid, new Circle(worldPos, component.RequiredFloorRadius), false))
-            {
-                if (!_turf.IsSpace(tile))
-                    continue;
-
-                var msg = Loc.GetString("nuke-component-cant-anchor-floor");
-                _popups.PopupEntity(msg, uid, args.Actor, PopupType.MediumCaution);
-
-                return;
-            }
-ES END */
-
-            _transform.SetCoordinates(uid, xform, xform.Coordinates.SnapToGrid());
-            _transform.AnchorEntity(uid, xform);
-            _itemSlots.SetLock(uid, component.DiskSlot, false);
-        }
-
-        UpdateUserInterface(uid, component);
-    }
 
     private void OnEnterButtonPressed(EntityUid uid, NukeComponent component, NukeKeypadEnterMessage args)
     {
