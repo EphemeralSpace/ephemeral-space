0c4690c79fce34d7aad991ff2665bf684e44c2c4
mirrorcult <19853115+mirrorcult@users.noreply.github.com>
2026-07-17 19:52:38 -0700

    wall spawner stuff

---
+++ Content.Server/_ES/Ratman/ESRatmanDoorVariationPassComponent.cs
@@ -0,0 +1,14 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._ES.Ratman;
+
+/// <inheritdoc cref="ESRatmanDoorVariationPassSystem"/>
+[RegisterComponent]
+public sealed partial class ESRatmanDoorVariationPassComponent : Component
+{
+    [DataField]
+    public EntProtoId Replacement = "SolidSecretDoorRatman";
+
+    [DataField]
+    public int Count = 5;
+}
