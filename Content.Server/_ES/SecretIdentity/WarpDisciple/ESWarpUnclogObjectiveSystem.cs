f3e3d76590fc86807cb8bc806df1e1e4c915574f
FyeStance <305721202+FyeStance@users.noreply.github.com>
2026-07-24 17:46:41 +0100

    warpdisc

---
+++ Content.Server/_ES/SecretIdentity/WarpDisciple/ESWarpUnclogObjectiveSystem.cs
@@ -0,0 +1,27 @@
+﻿using Content.Server._ES.Objectives;
+using Content.Server._ES.SecretIdentity.Objectives.Relays;
+using Content.Server._ES.SecretIdentity.Objectives.Relays.Components;
+using Content.Server._ES.SecretIdentity.WarpDisciple.Components;
+using Content.Server._ES.WarpDrive;
+using Content.Shared._ES.Objectives;
+
+namespace Content.Server._ES.SecretIdentity.WarpDisciple;
+
+public sealed partial class ESWarpUnclogObjectiveSystem : ESBaseObjectiveSystem<ESWarpUnclogObjectiveComponent>
+{
+    public override Type[] RelayComponents => new[] { typeof(ESWarpUnclogRelayComponent) };
+
+    [Dependency] private ESObjectiveSystem _objectives = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ESWarpUnclogObjectiveComponent, ESWarpDriveInterruptionClearedEvent>(OnInterruptionCleared);
+    }
+
+    private void OnInterruptionCleared(Entity<ESWarpUnclogObjectiveComponent> ent, ref ESWarpDriveInterruptionClearedEvent args)
+    {
+       _objectives.AdjustObjectiveCounter(ent.Owner);
+    }
+}
---
+++ Content.Shared/_ES/SecretIdentity/WarpDisciple/ESTeleportToWarpInstantAction.cs
@@ -0,0 +1,5 @@
+﻿using Content.Shared.Actions;
+
+namespace Content.Shared._ES.SecretIdentity.WarpDisciple;
+
+public sealed partial class ESTeleportToWarpInstantAction : InstantActionEvent;
