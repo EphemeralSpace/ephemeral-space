6a6b7c6495ca0147a7dfde712eeb31123df619a3
EmoGarbage404 <retron404@gmail.com>
2026-07-25 16:45:57 -0400

    Cannibal behavior

--- Content.Shared/_ES/Cryohusk/Components/ESCryohuskableComponent.cs
+++ Content.Shared/_ES/Cryohusk/Components/ESCryohuskableComponent.cs
@@ -1,15 +1,16 @@
 using Content.Shared.Polymorph;
 using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
-namespace Content.Server._ES.Cryohusk.Components;
+namespace Content.Shared._ES.Cryohusk.Components;
 
 /// <summary>
 /// Marks an entity as able to be converted into a cryohusk
 /// </summary>
-[RegisterComponent, AutoGenerateComponentPause]
-[Access(typeof(ESCryohuskSystem))]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentPause]
+[Access(typeof(ESSharedCryohuskSystem))]
 public sealed partial class ESCryohuskableComponent : Component
 {
     [DataField]
---
+++ Resources/Locale/en-US/_ES/secret-identities/crew/cannibal.ftl
@@ -0,0 +1,3 @@
+es-cannibal-popup-invalid = I can't eat that! I have standards!
+es-cannibal-popup-not-dead = They're still alive!
+es-cannibal-stagehand-notif = {$player} cannibalized {$other} and copied their secret identity!
