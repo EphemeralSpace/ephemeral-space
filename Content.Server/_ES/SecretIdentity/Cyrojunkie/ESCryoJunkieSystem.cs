96c79d9cdb9758389e3a88de94f1770f4524f1dc
EmoGarbage404 <retron404@gmail.com>
2026-07-27 16:24:09 -0400

    Add cryojunkie announcement

--- Content.Server/_ES/SecretIdentity/Cyrojunkie/ESCryoJunkieSystem.cs
+++ Content.Server/_ES/SecretIdentity/Cyrojunkie/ESCryoJunkieSystem.cs
@@ -1,16 +1,21 @@
 ﻿using Content.Server._ES.Cryohusk;
 using Content.Server._ES.Mind;
 using Content.Server.Atmos.EntitySystems;
+using Content.Server.Chat.Managers;
 using Content.Shared._ES.Core.Timer;
 using Content.Shared._ES.SecretIdentity.Cyrojunkie.Components;
 using Content.Shared.Administration.Systems;
 using Content.Shared.Atmos;
+using Content.Shared.Chat;
 using Content.Shared.Mind.Components;
+using Robust.Server.Player;
 
 namespace Content.Server._ES.SecretIdentity.Cyrojunkie;
 
 public sealed partial class ESCryoJunkieSystem : EntitySystem
 {
+    [Dependency] private IChatManager _chat = default!;
+    [Dependency] private IPlayerManager _player = default!;
     [Dependency] private ESCryohuskSystem _cryo = default!;
     [Dependency] private ESEntityTimerSystem _entityTimer = default!;
     [Dependency] private AtmosphereSystem _atmosphere = default!;
@@ -33,10 +38,15 @@ public sealed partial class ESCryoJunkieSystem : EntitySystem
 
     private void OnGhostAttempt(Entity<ESCryoJunkieMindComponent> ent, ref AutoGhostAttemptEvent args)
     {
-        if (args.Mind.Comp.CurrentEntity == null)
+        if (args.Mind.Comp.CurrentEntity is not { } owned)
             return;
 
-        _entityTimer.SpawnTimer(args.Mind.Comp.CurrentEntity.Value, ent.Comp.HuskDelay, new ESCyroJunkieTimerEvent());
+        var msg = Loc.GetString("es-cryojunkie-implant-notif");
+        var wrappedMsg = Loc.GetString("chat-manager-server-wrap-message", ("message", msg));
+        if (_player.TryGetSessionByEntity(owned, out var session))
+            _chat.ChatMessageToOne(ChatChannel.Server, msg, wrappedMsg, default, false, session.Channel, Color.LightBlue);
+
+        _entityTimer.SpawnTimer(owned, ent.Comp.HuskDelay, new ESCyroJunkieTimerEvent());
 
         args.Cancelled = true;
         RemComp(ent, ent.Comp);
--- Resources/Locale/en-US/_ES/species/species.ftl
+++ Resources/Locale/en-US/_ES/species/species.ftl
@@ -4,3 +4,5 @@ es-cryohusk-name = ???
 es-cryohusk-id-name = frozen ID card
 es-cryohusk-id-desc = It's practically a block of ice. Wonderful.
 es-cryohusk-convert-stagehand-notif = {$player} was turned into a cryohusk!
+
+es-cryojunkie-implant-notif = [font size=14]Your implant begins freezing away... In a short time, you will be [bold]revived[/bold] as a cryohusk![/font]
