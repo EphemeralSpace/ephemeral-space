51698f392e71fce27d7dcef3b3f55f6b5345e3bc
EmoGarbage404 <retron404@gmail.com>
2026-07-28 15:33:33 -0400

    FUCK LEAPLEECH AI FUCK THIS NOISE FUCK THIS WHOLE SYSTEM FUCK THE WORLD AND MOST OF ALL FUCK YOU!

---
+++ Content.Server/_ES/NPCs/Queries/Considerations/ESTargetInUnobstructedPathCon.cs
@@ -0,0 +1,8 @@
+using Content.Server.NPC.Queries.Considerations;
+
+namespace Content.Server._ES.NPCs.Queries.Considerations;
+
+public sealed partial class ESTargetInUnobstructedPathCon : UtilityConsideration
+{
+
+}
