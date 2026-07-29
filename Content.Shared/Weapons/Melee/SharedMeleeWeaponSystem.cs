c004caf0667764ca0bad879d94213639c962e427
mirrorcult <19853115+mirrorcult@users.noreply.github.com>
2026-07-29 15:07:10 +0000

    separate melee slowdowns for attack/getting hit (#2347)

--- Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
+++ Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
@@ -72,7 +72,8 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
     [Dependency] private StatusEffectNew.StatusEffectsSystem _status = default!;
     // ES END
 
-    private static readonly EntProtoId MeleeSlowStatusEffect = "ESMeleeTemporarySlowdown";
+    private static readonly EntProtoId MeleeAttackSlowStatusEffect = "ESMeleeTemporarySlowdownAttack";
+    private static readonly EntProtoId MeleeDamageSlowStatusEffect = "ESMeleeTemporarySlowdownDamage";
 
     private const int AttackMask = (int) (CollisionGroup.MobMask | CollisionGroup.Opaque);
 
@@ -542,7 +543,7 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
             var missEvent = new MeleeHitEvent(new List<EntityUid>(), user, meleeUid, damage, null);
             RaiseLocalEvent(meleeUid, missEvent);
             _meleeSound.PlaySwingSound(user, meleeUid, component);
-            _status.TrySetStatusEffectDuration(user, MeleeSlowStatusEffect, TimeSpan.FromSeconds(0.35));
+            _status.TrySetStatusEffectDuration(user, MeleeAttackSlowStatusEffect, TimeSpan.FromSeconds(0.35));
             return;
         }
 
@@ -569,7 +570,7 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
         // somewhat messy scuffle. See also, heavy attacks.
         Interaction.DoContactInteraction(user, target, weapon, true, interactionParticles: false); // Stellar/ES - Interaction particles
 
-        _status.TrySetStatusEffectDuration(user, MeleeSlowStatusEffect, TimeSpan.FromSeconds(0.35));
+        _status.TrySetStatusEffectDuration(user, MeleeAttackSlowStatusEffect, TimeSpan.FromSeconds(0.35));
 
         // For stuff that cares about it being attacked.
         var attackedEvent = new AttackedEvent(meleeUid, user, targetXform.Coordinates);
@@ -607,7 +608,7 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
         if (damageResult.GetTotal() > FixedPoint2.Zero)
         {
             DoDamageEffect(targets, user, targetXform);
-            _status.TrySetStatusEffectDuration(target.Value, MeleeSlowStatusEffect, TimeSpan.FromSeconds(0.35));
+            _status.TrySetStatusEffectDuration(target.Value, MeleeDamageSlowStatusEffect, TimeSpan.FromSeconds(0.35));
 
             // ES START
             // dog shit copy plaste but thats melee for you
@@ -662,7 +663,7 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
             RaiseLocalEvent(meleeUid, missEvent);
 
             // immediate audio feedback
-            _status.TrySetStatusEffectDuration(user, MeleeSlowStatusEffect, TimeSpan.FromSeconds(0.35));
+            _status.TrySetStatusEffectDuration(user, MeleeAttackSlowStatusEffect, TimeSpan.FromSeconds(0.35));
             _meleeSound.PlaySwingSound(user, meleeUid, component);
 
             return true;
@@ -717,7 +718,7 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
         var weapon = GetEntity(ev.Weapon);
 
         Interaction.DoContactInteraction(user, weapon, null, true, interactionParticles: false); // Stellar - Interaction particles
-        _status.TrySetStatusEffectDuration(user, MeleeSlowStatusEffect, TimeSpan.FromSeconds(0.35));
+        _status.TrySetStatusEffectDuration(user, MeleeAttackSlowStatusEffect, TimeSpan.FromSeconds(0.35));
 
         // For stuff that cares about it being attacked.
         foreach (var target in targets)
@@ -759,7 +760,7 @@ public abstract partial class SharedMeleeWeaponSystem : EntitySystem
                 }
 
                 appliedDamage += damageResult;
-                _status.TrySetStatusEffectDuration(entity, MeleeSlowStatusEffect, TimeSpan.FromSeconds(0.35));
+                _status.TrySetStatusEffectDuration(entity, MeleeDamageSlowStatusEffect, TimeSpan.FromSeconds(0.35));
 
                 if (meleeUid == user)
                 {
