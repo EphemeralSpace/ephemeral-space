3f6f8e11f0b691a9ebb6e70863f9ce7a7de95d8f
EmoGarbage404 <retron404@gmail.com>
2026-08-14 01:34:54 -0400

    YUCK legacy

--- Content.Shared/_ES/Chat/Components/ESSpeechChatChannelComponent.cs
+++ Content.Shared/_ES/Chat/Components/ESSpeechChatChannelComponent.cs
@@ -3,10 +3,10 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._ES.Chat.Components;
 
 /// <summary>
-/// Used for a chat channel which represents physical speech spoken by players.
+/// Legacy behavior for <see cref="ESChatProcessorComponent"/> that covers transformations for various "spoken" behavior.
+/// This is not really a logical distinction, but it can be though to handle any chat channel in which a person physically "speaks."
+/// So things like accents, voice obfuscation, and events relating to "speaking" are handled here. For Now.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class ESSpeechChatChannelComponent : Component
-{
-
-}
+[Access(typeof(ESSpeechSystem))]
+public sealed partial class ESSpeechChatChannelComponent : Component;
